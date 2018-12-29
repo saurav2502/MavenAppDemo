3aa129d71c8913eaa99b105de1c2a000c72fa390
saurav2502 <sauravstarsk@gmail.com>
2018-12-29 20:18:26 +0530

    added new

--- src/test/java/sampleTest.java
+++ src/test/java/sampleTest.java
@@ -7,7 +7,7 @@ public class sampleTest {
     public void sampleFail(){
         int a=0;
         int b=4;
-        assertTrue(a+b == 5);
+        assertTrue(a+b == 4);
     }
     @Test
     public void samplePass(){
