8a93dbd6511381d65c48e9deaea45a880c56c23d
saurav2502 <sauravstarsk@gmail.com>
2018-12-29 16:33:18 +0530

    Initial commit

---
+++ src/test/java/sampleTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class sampleTest {
+    @Test
+    public void sampleFail(){
+        int a=0;
+        int b=4;
+        assertTrue(a+b == 5);
+    }
+    @Test
+    public void samplePass(){
+        int a=0;
+        int b=5;
+        assertTrue(a+b == 5);
+    }
+}
